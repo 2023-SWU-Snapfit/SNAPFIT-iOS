89f0ce114c534d6b773ed8af9c300a171e4eafcb
dev-madilyn <lollin0@naver.com>
2023-05-02 03:25:57 +0900

    Create: 프로젝트 생성

---
+++ SNAPFIT/SNAPFIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SNAPFIT
+//
+//  Created by madilyn on 2023/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
