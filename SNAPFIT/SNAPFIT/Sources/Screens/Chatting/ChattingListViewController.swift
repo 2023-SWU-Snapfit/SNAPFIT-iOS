7706510997b362ceceb7d8465f0904542a4410b6
Hamthoven <hamthoven@gmail.com>
2023-05-12 15:45:55 +0900

    [FEAT] ViewController 생성 #10
    - ChattingViewController 생성
    - HeartListViewController 생성

---
+++ SNAPFIT/SNAPFIT/Sources/Screens/Chatting/ChattingListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingListViewController.swift
+//  SNAPFIT
+//
+//  Created by 강유진 on 2023/05/12.
+//
+
+import UIKit
+import SnapKit
+
+class ChattingListViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ SNAPFIT/SNAPFIT/Sources/Screens/HeartListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HeartListViewController.swift
+//  SNAPFIT
+//
+//  Created by 강유진 on 2023/05/12.
+//
+
+import UIKit
+
+class HeartListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
