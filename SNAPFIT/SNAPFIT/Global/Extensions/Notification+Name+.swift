df8c4ad900ab2f234850a7111d915bd8f962014a
dev-madilyn <lollin0@naver.com>
2023-05-14 14:12:39 +0900

    Create: Extension 파일들 추가 #12

---
+++ SNAPFIT/SNAPFIT/Global/Extensions/Notification+Name+.swift
@@ -0,0 +1,12 @@
+//
+//  Notification+Name+.swift
+//  SNAPFIT
+//
+//  Created by madilyn on 2023/05/14.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let sendViewState = Notification.Name("sendViewState")
+}
