d0b36554ba95d41657b055dd62b07cd913768443
Hamthoven <hamthoven@gmail.com>
2023-05-22 16:31:21 +0900

    사진작가 마이페이지 텍스트 내용 없을 경우 아예 보이지 않음

--- SNAPFIT/SNAPFIT/Sources/Components/SnapfitTextView.swift
+++ SNAPFIT/SNAPFIT/Sources/Components/SnapfitTextView.swift
@@ -6,15 +6,50 @@
 //
 
 import UIKit
+import SnapKit
 
 class SnapfitTextView: UITextView {
-   
-    func setBasicTextbox() {
+    
+    public init(isEditable: Bool) {
+        super.init(frame: .zero, textContainer: nil)
+        if isEditable {
+            setEditableStyle()
+        } else {
+            setNotEditableStyle()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setClearStyle() {
+        self.sizeToFit()
+        self.backgroundColor = .clear
+        self.layer.borderWidth = 0
+        self.textContainerInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func setEditableStyle() {
+        self.font = .r14
+        self.textAlignment = .natural
+        self.isEditable = true
+        self.isScrollEnabled = false
+        self.sizeToFit()
+        self.backgroundColor = .sfWhite
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 8
+        self.layer.borderColor = UIColor.sfBlack60.cgColor
+        self.textContainerInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    }
+    
+    func setNotEditableStyle() {
         self.font = .r14
         self.textAlignment = .natural
         self.isEditable = false
         self.isScrollEnabled = false
         self.sizeToFit()
+        self.backgroundColor = .sfWhite
         self.layer.borderWidth = 1
         self.layer.cornerRadius = 8
         self.layer.borderColor = UIColor.sfBlack60.cgColor
@@ -22,6 +57,9 @@ class SnapfitTextView: UITextView {
     }
     
     func setText(text: String = "") {
+        if text == "" {
+            setClearStyle()
+        }
         self.text = text
     }
 }
--- SNAPFIT/SNAPFIT/Sources/Screens/Mypage/MypagePhotographerViewController.swift
+++ SNAPFIT/SNAPFIT/Sources/Screens/Mypage/MypagePhotographerViewController.swift
@@ -9,75 +9,104 @@ import UIKit
 import SnapKit
 
 class MypagePhotographerViewController: BaseViewController {
-    // MARK: - Properties
-    let scrollView = UIScrollView()
-    let contentView = UIView()
-    let bannerImageView = UIImageView()
-    let profileImageView = UIImageView()
-    let phoneApprovedLabel = UILabel()
-    let nicknameLabel = UILabel()
-    let mailLabel = UILabel()
-    let introduceTextView = TextboxView()
-    let galleryCarouselViewController = GalleryCollectionlViewController()
-    let reviewCarouselViewController = ReviewCollectionlViewController()
-    let possibleDateTitleLabel = UILabel()
-    let possibleDateTextView = TextboxView()
-    let priceTitleLabel = UILabel()
-    let priceTextView = TextboxView()
+    
+    enum MypageText {
+        static let phoneApproved = "휴대폰 인증 완료"
+        static let nickname = "열글자까지가능한이름"
+        static let possibleDate = "예약 가능 날짜"
+        static let price = "촬영 비용"
+    }
+    
+    private let scrollView: UIScrollView = {
+        let scrollView: UIScrollView = UIScrollView()
+        scrollView.backgroundColor = .sfGrayWhite
+        scrollView.showsVerticalScrollIndicator = false
+        return scrollView
+    }()
+    private let contentView: UIView = UIView()
+    private let bannerImageView: UIImageView = {
+        let bannerImageView: UIImageView = UIImageView()
+        bannerImageView.setImageColor(color: .sfBlack40)
+        bannerImageView.backgroundColor = .sfBlack20
+        return bannerImageView
+        
+    }()
+    private let profileImageView: UIImageView = {
+        let profileImageView: UIImageView = UIImageView()
+        return profileImageView
+    }()
+    private let phoneApprovedLabel: UILabel = {
+        let phoneApprovedLabel: UILabel = UILabel()
+        phoneApprovedLabel.text = MypageText.phoneApproved
+        phoneApprovedLabel.font = .m12
+        return phoneApprovedLabel
+    }()
+    private let nicknameLabel: UILabel = {
+        let nicknameLabel: UILabel = UILabel()
+        nicknameLabel.text = MypageText.nickname
+        nicknameLabel.font = .m24
+        return nicknameLabel
+
+    }()
+    private let mailLabel: UILabel = {
+        let mailLabel: UILabel = UILabel()
+        mailLabel.font = .m12
+        return mailLabel
+    }()
+    private let introduceTextView: SnapfitTextView = {
+        let introduceTextView: SnapfitTextView = SnapfitTextView(isEditable: false)
+        return introduceTextView
+    }()
+    private let galleryCarouselViewController: GalleryCollectionlViewController = GalleryCollectionlViewController()
+    private let reviewCarouselViewController: ReviewCollectionlViewController = ReviewCollectionlViewController()
+    private let possibleDateTitleLabel: UILabel = {
+        let possibleDateTitleLabel: UILabel = UILabel()
+        possibleDateTitleLabel.text = MypageText.possibleDate
+        possibleDateTitleLabel.font = .b18
+        return possibleDateTitleLabel
+    }()
+    private let possibleDateTextView: SnapfitTextView = {
+        let possibleDateTextView: SnapfitTextView = SnapfitTextView(isEditable: false)
+        return possibleDateTextView
+    }()
+    private let priceTitleLabel: UILabel = {
+        let priceTitleLabel: UILabel = UILabel()
+        priceTitleLabel.text = MypageText.price
+        priceTitleLabel.font = .b18
+        return priceTitleLabel
+    }()
+    private let priceTextView: SnapfitTextView = {
+        let priceTextView: SnapfitTextView = SnapfitTextView(isEditable: false)
+        return priceTextView
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // MARK: - Initialization
-        scrollView.backgroundColor = .sfGrayWhite
-        scrollView.showsVerticalScrollIndicator = false
         
-        bannerImageView.setImageColor(color: .sfBlack40)
-        self.bannerImageView.backgroundColor = .sfBlack20
-        
-        profileImageView.setImageColor(color: .sfBlack40)
-        self.profileImageView.backgroundColor = .sfBlack40
-        self.profileImageView.layer.cornerRadius = 50
-        self.profileImageView.layer.borderWidth = 1
-        self.profileImageView.layer.borderColor = UIColor.clear.cgColor
-        self.profileImageView.clipsToBounds = true
-        self.profileImageView.contentMode = .scaleAspectFill
-        
-        self.phoneApprovedLabel.text = "휴대폰 인증 완료"
-        self.phoneApprovedLabel.font = .m12
-        
-        self.nicknameLabel.text = "열글자까지가능한이름"
-        self.nicknameLabel.font = .m24
-        
-        self.mailLabel.text = "e-mailLabel.emailLabel.com"
-        self.mailLabel.font = .m12
-        
-        self.introduceTextView.setBasicTextbox()
-        self.introduceTextView.text = """
+        self.setProfileImageViewStyle()
+        self.setLayout()
+        self.setmailText(text: "e-mail.emailLabel.com")
+        self.setIntroduceText(text: """
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    
-    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    일부러 길게 적어봤다. 과연 어떻게 보일지 궁금하기도 하고... 한국어 대응은 잘 되는군! 일단 멀티라인 스트링이 당연히 필요할테니 길게... 사용해봤다
-    그리고 이렇게 길게 썼는데도 스크롤 멀쩡히 작동하니 만족!
+
+    한국어 대응 가능
     """
-        
-        self.possibleDateTitleLabel.text = "예약 가능 날짜"
-        self.possibleDateTitleLabel.font = .b18
-        
-        self.possibleDateTextView.setBasicTextbox()
-        self.possibleDateTextView.text = """
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                             )
+        self.setPossibleDateText(text: """
+    멀티라인 스트링 입력 가능
     AA - $200
     AB - $60
     """
-        self.priceTitleLabel.text = "촬영 비용"
-        self.priceTitleLabel.font = .b18
-        
-        self.priceTextView.setBasicTextbox()
-        self.priceTextView.text = """
+                                 )
+        self.setPriceText(text: """
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
     """
-        
-        // MARK: - UI
+                          )
+    }
+    
+    private func setLayout() {
+        // MARK: - Layout
         self.view.addSubview(self.scrollView)
         self.scrollView.snp.makeConstraints{ make in
             make.edges.width.equalToSuperview()
@@ -153,5 +182,36 @@ class MypagePhotographerViewController: BaseViewController {
             make.left.equalTo(20)
             make.width.bottom.equalToSuperview().inset(20)
         }
+    }
+    
+    private func setProfileImageViewStyle() {
+        self.profileImageView.setImageColor(color: .sfBlack40)
+        self.profileImageView.backgroundColor = .sfBlack40
+        self.profileImageView.layer.cornerRadius = 50
+        self.profileImageView.layer.borderWidth = 1
+        self.profileImageView.layer.borderColor = UIColor.clear.cgColor
+        self.profileImageView.clipsToBounds = true
+        self.profileImageView.contentMode = .scaleAspectFill
+    }
+
+    public func setmailText(text: String) {
+        self.mailLabel.text = text
+    }
+
+    public func setIntroduceText(text: String) {
+        self.introduceTextView.setText(text: text)
+    }
+
+    public func setPossibleDateText(text: String) {
+        if text == "" {
+            self.possibleDateTitleLabel.isHidden = true
+        }
+        self.possibleDateTextView.setText(text: text)
+    }
+    public func setPriceText(text: String) {
+        if text == "" {
+            self.priceTitleLabel.isHidden = true
+        }
+        self.priceTextView.setText(text: text)
     }
 }
