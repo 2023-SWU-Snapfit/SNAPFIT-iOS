de82202dc0b358c660031d242a32ee41ac964f47
dev-madilyn <lollin0@naver.com>
2023-05-25 10:18:53 +0900

    Feat: 네비게이션 영역 구현 #21

--- SNAPFIT/SNAPFIT/Sources/Screens/Home/HomeViewController.swift
+++ SNAPFIT/SNAPFIT/Sources/Screens/Home/HomeViewController.swift
@@ -62,6 +62,50 @@ extension HomeViewController {
     }
 }
 
+// MARK: - UITableViewDataSource
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TableRow.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let rowType = TableRow(rawValue: indexPath.row) {
+            switch rowType {
+            case .navigation:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeNavigationTableViewCell.className) as? HomeNavigationTableViewCell else { return HomeNavigationTableViewCell() }
+                cell.navigationView.notificationButton.removeTarget(nil, action: nil, for: .allEvents)
+                cell.navigationView.notificationButton.setAction { [weak self] in
+                    self?.navigationController?.pushViewController(NotificationViewController(), animated: true)
+                }
+                return cell
+            case .reservationDetail:
+                return UITableViewCell()
+            case .searchBar:
+                return UITableViewCell()
+            case .categoryTag:
+                return UITableViewCell()
+            case .photoBySelectedCategory:
+                return UITableViewCell()
+            case .personalCategoryTitle:
+                return UITableViewCell()
+            case .photoByPersonalCategory:
+                return UITableViewCell()
+            case .bestPhotographerTitle:
+                return UITableViewCell()
+            case .bestPhotographerList:
+                return UITableViewCell()
+            case .themeTitle:
+                return UITableViewCell()
+            case .photoByTheme:
+                return UITableViewCell()
+            }
+        } else { return UITableViewCell() }
+    }
+}
+
+// MARK: - UITableViewDelegate
+
 extension HomeViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if let rowType = TableRow(rawValue: indexPath.row) {
