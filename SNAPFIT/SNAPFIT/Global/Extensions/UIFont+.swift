32368e1955664e2bc762a578fe251f6948c3db4e
Hamthoven <hamthoven@gmail.com>
2023-05-29 10:14:25 +0900

    UIFont+에 b24 추가

--- SNAPFIT/SNAPFIT/Global/Extensions/UIFont+.swift
+++ SNAPFIT/SNAPFIT/Global/Extensions/UIFont+.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 extension UIFont {
+    
+    class var b24: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 24.0)!
+    }
+    
     class var m24: UIFont {
         return UIFont(name: "Pretendard-Medium", size: 24.0)!
     }
