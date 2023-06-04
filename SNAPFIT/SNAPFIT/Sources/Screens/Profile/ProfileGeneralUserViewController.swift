ac745c7e2520f23ee8e9f2a1d3064dafaecc9178
Hamthoven <hamthoven@gmail.com>
2023-06-04 09:57:52 +0900

    일반 사용자용 마이페이지, 프로필 뷰 생성

---
+++ SNAPFIT/SNAPFIT/Sources/Screens/Profile/ProfileGeneralUserViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ProfileGeneralUserViewController.swift
+//  SNAPFIT
+//
+//  Created by 강유진 on 2023/06/04.
+//
+
+import UIKit
+import SnapKit
+
+class ProfileGeneralUserViewController: SnapfitUserInformationViewController {
+    
+    let contactButton: UIButton = {
+        let contactButton: UIButton = UIButton()
+        contactButton.configuration = .filled()
+        contactButton.setTitle("문의하기", for: .normal)
+        contactButton.titleLabel?.font = .b14
+        contactButton.titleLabel?.textColor = .sfWhite
+        contactButton.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16)
+        contactButton.tintColor = .sfBlack100
+        contactButton.makeRounded(cornerRadius: 10)
+        return contactButton
+    }()
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setPhotographerLayout()
+        self.setMypageLayout()
+        self.setMailText(text: "e-mail.emailLabel.com")
+        self.setIntroduceText(text: """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+    한국어 대응 가능
+    """
+                             )
+        self.setPossibleDateText(text: """
+    멀티라인 스트링 입력 가능
+    AA - $200
+    AB - $60
+    """
+                                 )
+        self.setPriceText(text: """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    """
+                          )
+    }
+    
+    
+    private func setMypageLayout() {
+        self.addAtContentView(component: self.contactButton)
+        self.contactButton.snp.makeConstraints{ make in
+            make.top.equalTo(338)
+            make.right.equalToSuperview().inset(20)
+        }
+    }
+    
+}
