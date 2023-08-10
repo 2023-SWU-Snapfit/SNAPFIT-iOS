07e266d110d6d0a737cd54ab45a25df3e083bab5
Hamthoven <hamthoven@gmail.com>
2023-08-10 22:50:59 +0900

    예약 관리 탭 예약 확정 & 문의 중으로 탭 구분

--- SNAPFIT/SNAPFIT/Sources/Screens/Reservation/ReservationViewController.swift
+++ SNAPFIT/SNAPFIT/Sources/Screens/Reservation/ReservationViewController.swift
@@ -10,8 +10,35 @@ import UIKit
 final class ReservationViewController: BaseViewController {
     // MARK: - Properties
     var dataDelegate: ReservationDataDelegate?
+    var isFixedTab: Bool = true
+    
+    // FIXME: [DATABASE] DB 도입 시 cellForRowAt 정리
+    var fixedReservation: [Reservation] = []
+    var notFixedReservation: [Reservation] = []
     
     // MARK: - UIComponents
+    let fixedTabButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.configuration = .filled()
+        button.titleLabel?.textColor = .sfWhite
+        let attributes = [NSAttributedString.Key.font : UIFont.b14]
+        button.setAttributedTitle(NSAttributedString(string: "예약 확정", attributes: attributes), for: .normal)
+        button.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16)
+        button.tintColor = .sfBlack100
+        button.makeRounded(cornerRadius: 8)
+        return button
+    }()
+    let notFixedTabButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.configuration = .filled()
+        button.titleLabel?.textColor = .sfWhite
+        let attributes = [NSAttributedString.Key.font : UIFont.b14]
+        button.setAttributedTitle(NSAttributedString(string: "문의 중", attributes: attributes), for: .normal)
+        button.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16)
+        button.tintColor = .sfBlack100
+        button.makeRounded(cornerRadius: 8)
+        return button
+    }()
     let reservationTableView: UITableView = {
         let reservationTableView: UITableView = UITableView()
             reservationTableView.backgroundColor = .clear
@@ -24,7 +51,11 @@ final class ReservationViewController: BaseViewController {
     // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.fixedReservation = reservationData.filter({ $0.isFixed == true })
+        self.notFixedReservation = reservationData.filter({ $0.isFixed == false })
+        self.reservationTableView.reloadData()
         self.setNavigationTitle()
+        self.setTabStateButtons()
         self.setTableView()
     }
     
@@ -50,13 +81,61 @@ final class ReservationViewController: BaseViewController {
         self.navigationController?.navigationBar.isHidden = false
     }
     
+    private func setTabStateButtons() {
+        self.setTabStateButtonStyle()
+        self.setTabButtonAction()
+        
+        self.view.addSubview(self.fixedTabButton)
+        self.fixedTabButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(160)
+            make.leading.equalToSuperview().offset(20)
+            make.height.equalTo(32)
+        }
+        self.view.addSubview(self.notFixedTabButton)
+        self.notFixedTabButton.snp.makeConstraints { make in
+            make.top.equalTo(self.fixedTabButton)
+            make.leading.equalTo(self.fixedTabButton.snp.trailing).offset(16)
+            make.height.equalTo(32)
+        }
+    }
+    
+    private func setTabStateButtonStyle() {
+        self.fixedTabButton.setTitle("예약 확정", for: .normal)
+        self.notFixedTabButton.setTitle("문의 중", for: .normal)
+        if self.isFixedTab {
+            self.fixedTabButton.configuration = .filled()
+            self.notFixedTabButton.configuration = .tinted()
+        } else {
+            self.fixedTabButton.configuration = .tinted()
+            self.notFixedTabButton.configuration = .filled()
+        }
+    }
+    
+    func setTabButtonAction() {
+        self.fixedTabButton.setAction {
+            if self.isFixedTab { return }
+            self.isFixedTab.toggle()
+            print(self.isFixedTab)
+            self.setTabStateButtonStyle()
+            self.reservationTableView.reloadData()
+        }
+
+        self.notFixedTabButton.setAction {
+            if !self.isFixedTab { return }
+            self.isFixedTab.toggle()
+            print(self.isFixedTab)
+            self.setTabStateButtonStyle()
+            self.reservationTableView.reloadData()
+        }
+    }
+    
     private func setTableView() {
         self.reservationTableView.delegate = self
         self.reservationTableView.dataSource = self
         self.view.addSubview(reservationTableView)
         self.reservationTableView.snp.makeConstraints{ make in
-            make.top.equalToSuperview().offset(16)
-            make.bottom.equalToSuperview()
+            make.top.equalTo(self.fixedTabButton.snp.bottom).offset(16)
+            make.bottom.equalToSuperview().inset(16)
             make.left.right.equalToSuperview().inset(20)
         }
     }
@@ -72,25 +151,37 @@ extension ReservationViewController: UITableViewDelegate {
         lazy var detailViewController: ReservationDetailViewController = ReservationDetailViewController()
         detailViewController.modalPresentationStyle = .fullScreen
         self.dataDelegate = detailViewController
-        dataDelegate?.recieveReservationData(reservationData: reservationData[indexPath.row])
+        let data = self.isFixedTab ? fixedReservation[indexPath.row] : notFixedReservation[indexPath.row]
+        dataDelegate?.recieveReservationData(reservationData: data)
         self.navigationController?.present(detailViewController, animated: true)
     }
 }
 
 extension ReservationViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return reservationData.count
+        return reservationData.filter({ $0.isFixed == isFixedTab }).count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let user = reservationData[indexPath.row].senderID == 133 ? users[reservationData[indexPath.row].recieverID] : users[reservationData[indexPath.row].senderID]
-        
+        var user: User
         let cell = tableView.dequeueReusableCell(withIdentifier: "reservationTableViewCell") as! ReservationTableViewCell
-        cell.setAsReservationList(
-            userName: "\(user.userName)",
-            lastDate: reservationData[indexPath.row].lastUpdate,
-            isFixed: reservationData[indexPath.row].isFixed,
-            isFinished: reservationData[indexPath.row].isFinished
-        )
+        
+        if self.isFixedTab {
+            user = fixedReservation[indexPath.row].senderID == 133 ? users[fixedReservation[indexPath.row].recieverID] : users[fixedReservation[indexPath.row].senderID]
+            cell.setAsReservationList(
+                userName: "\(user.userName)",
+                lastDate: fixedReservation[indexPath.row].lastUpdate,
+                isFixed: fixedReservation[indexPath.row].isFixed,
+                isFinished: fixedReservation[indexPath.row].isFinished
+            )
+        } else {
+            user = notFixedReservation[indexPath.row].senderID == 133 ? users[notFixedReservation[indexPath.row].recieverID] : users[notFixedReservation[indexPath.row].senderID]
+            cell.setAsReservationList(
+                userName: "\(user.userName)",
+                lastDate: notFixedReservation[indexPath.row].lastUpdate,
+                isFixed: notFixedReservation[indexPath.row].isFixed,
+                isFinished: notFixedReservation[indexPath.row].isFinished
+            )
+        }
         return cell
     }
 }
