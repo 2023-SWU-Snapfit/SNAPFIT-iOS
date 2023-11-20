8da67ea1637b1b89794ccc4e58db5630e5c408fe
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-11-20 20:41:17 +0900

    Chore: UserDefaults Manager, Key 추가 (email, password) #40

--- SNAPFIT/SNAPFIT/Global/Utils/UserDefaultsManager.swift
+++ SNAPFIT/SNAPFIT/Global/Utils/UserDefaultsManager.swift
@@ -12,4 +12,14 @@ struct UserDefaultsManager {
         get { return UserDefaults.standard.integer(forKey: UserDefaults.Keys.userID) }
         set { UserDefaults.standard.set(newValue, forKey: UserDefaults.Keys.userID) }
     }
+    
+    static var email: String? {
+        get { return UserDefaults.standard.string(forKey: UserDefaults.Keys.email) }
+        set { UserDefaults.standard.set(newValue, forKey: UserDefaults.Keys.email) }
+    }
+    
+    static var password: String? {
+        get { return UserDefaults.standard.string(forKey: UserDefaults.Keys.password) }
+        set { UserDefaults.standard.set(newValue, forKey: UserDefaults.Keys.password) }
+    }
 }
