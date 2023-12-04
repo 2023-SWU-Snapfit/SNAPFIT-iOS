3123ea9b56d60e0902866e7cafe17e98f4c0b15c
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2023-12-05 00:11:43 +0900

    Chore: getImage 함수 수정 #45

--- SNAPFIT/SNAPFIT/Global/Extensions/String+.swift
+++ SNAPFIT/SNAPFIT/Global/Extensions/String+.swift
@@ -11,19 +11,19 @@ import UIKit.UIImage
 extension String {
     func getImage(completion: @escaping (UIImage) -> ()){
         let cacheKey = NSString(string: self)
+        var resultImage = UIImage()
         
         /// 해당 Key에 캐시 이미지가 저장되어 있으면 이미지 사용
         if let cachedImage = ImageCacheManager.shared.object(forKey: cacheKey) {
-            completion(cachedImage)
+            resultImage = cachedImage
+            completion(resultImage)
         }
         
         if let requestURL = URL(string: self) {
             let request = URLRequest(url: requestURL)
             
             URLSession.shared.dataTask(with: request) { data, response, error in
-                if let data = data,
-                   let response = response, ((response as? HTTPURLResponse)?.statusCode ?? 500) < 300,
-                   let image = UIImage(data: data) {
+                if let data = data, let response = response, ((response as? HTTPURLResponse)?.statusCode ?? 500) < 300, let image = UIImage(data: data) {
                     
                     /// 다운받은 이미지를 캐시에 저장
                     ImageCacheManager.shared.setObject(image, forKey: cacheKey)
